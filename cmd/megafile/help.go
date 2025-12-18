792a4d838d48c6a6d4f42e127a94f9b28507e04d
Alexander F. Rødseth <rodseth@gmail.com>
2025-12-18 16:31:01 +0100

    Update documentation

--- cmd/megafile/help.go
+++ cmd/megafile/help.go
@@ -12,20 +12,43 @@ q, quit or exit     exit program
 
 Hotkeys:
 
-tab                 cycle between the 3 different current diretories,
-                    or tab completion of directories and filenames.
-ctrl-space          cycle to the previous directory
-ctrl-q              exit program
-ctrl-h or ctrl-o    toggle "show hidden files"
-ctrl-a              start of line
-ctrl-d              delete character under cursor, or exit program
-ctrl-k              delete text to the end of the line
-ctrl-l              clear screen
-ctrl-c              clear text, or exit program
-ctrl-t              run tig
-ctrl-g              run lazygit
-ctrl-n              enter the freshest directory
-ctrl-p              go up one directory
+Navigation and Selection:
+  arrow keys        navigate and select files (up/down/left/right)
+  page up/down      jump to first/last entry in current column
+  home or ctrl-a    jump to first file (or start of line when typing)
+  end or ctrl-e     jump to last file (or end of line when typing)
+
+Execution:
+  return            execute selected file, or run typed command
+  esc               clear selection (first press), exit program (second press)
+
+Text Editing:
+  backspace         delete character, or go up directory (when at start)
+  ctrl-h            delete character, or toggle hidden files (when at start)
+  ctrl-d            delete character under cursor, or exit program
+  ctrl-k            delete text to the end of the line
+  ctrl-c            clear text, or exit program
+
+File Operations:
+  tab               cycle through files, or tab completion
+  ctrl-f            search for text in files
+
+Directory Navigation:
+  ctrl-space        enter the most recent subdirectory
+  ctrl-n            cycle to next directory
+  ctrl-p            cycle to previous directory
+  ctrl-b            go to parent directory
+
+Display:
+  ctrl-o            toggle show hidden files
+  ctrl-l            clear screen
+
+External Tools:
+  ctrl-t            run tig
+  ctrl-g            run lazygit
+
+Exit:
+  ctrl-q            exit program immediately
 
 Flags:
 
